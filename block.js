894e6ac12d20e26b2ed56c1b4dfcc6c93282e5e6
DESKTOP-MGIULOS\starman <starman3ch@gmail.com>
2018-07-15 17:32:28 +0900

    SHA256 Hash Function

--- block.js
+++ block.js
@@ -1,3 +1,5 @@
+const SHA256 = require('crypto-js/sha256');
+
 class Block {
     constructor(timestamp, lastHash, hash, data) {
         this.timestamp = timestamp;
@@ -9,8 +11,8 @@ class Block {
     toString() {
         return `Block -
             Timestamp: ${this.timestamp}
-            Last Hash: ${this.lastHash}
-            Hash     : ${this.hash}
+            Last Hash: ${this.lastHash.substring(0, 10)}
+            Hash     : ${this.hash.substring(0, 10)}
             Data     : ${this.data}`;
     }
 
@@ -21,9 +23,13 @@ class Block {
     static mineBlock(lastBlock, data) {
         const timestamp = Date.now();
         const lastHash = lastBlock.hash;
-        const hash = 'todo-hash';
+        const hash = Block.hash(timestamp, lastHash, data);
 
         return new this(timestamp, lastHash, hash, data);
+    }
+
+    static hash(timestamp, lastHash, data) {
+        return SHA256(`${timestamp}${lastHash}${data}`).toString();
     }
 }
 
