37cb8cc1e02319a16b09dba2cd96689d4a9ec2d1
starman3ch <starman3ch@gmail.com>
2018-09-21 21:55:35 +0900

    Test the Transaction Input

--- wallet/transaction.js
+++ wallet/transaction.js
@@ -29,7 +29,7 @@ class Transaction {
             timestamp: Date.now(),
             amount: senderWallet.balance,
             address: senderWallet.publicKey,
-            signature: senderWAllet.sign(ChainUtil.hash(transaction.outputs))
+            signature: senderWallet.sign(ChainUtil.hash(transaction.outputs))
         }
     }
 }
