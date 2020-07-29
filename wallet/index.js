7ed151c943fdf04e39860223579ee0a6743cc73e
starman <starman3ch@gmail.com>
2020-07-29 16:19:18 +0900

    Calculate the Balance during each Transaction

--- wallet/index.js
+++ wallet/index.js
@@ -19,7 +19,9 @@ class Wallet {
         return this.keyPair.sign(dataHash);
     }
 
-    createTransaction(recipient, amount, transactionPool) {
+    createTransaction(recipient, amount, blockchain, transactionPool) {
+        this.balance = this.calculateBalance(blockchain);
+
         if (amount > this.balance) {
             console.log(`Amount: ${amount} exceeds current balance: ${this.balance}`);
             return;
@@ -59,7 +61,7 @@ class Wallet {
         }
 
         transactions.forEach(transaction => {
-            if (transactions.input.timestamp > startTime) {
+            if (transaction.input.timestamp > startTime) {
                 transaction.outputs.find(output => {
                     if (output.address === this.publicKey) {
                         balance += output.amount;
